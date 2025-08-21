b9623b072db5ba6bded17d73e7b0b64b95cff40c
mohd <generalassemblyse@gmail.com>
2025-08-21 16:34:18 +0300

    create cat

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView
 from .models import Cat
 # Create your views here.
 
-
+# FUNCTION BASED VIEWS
 def home(request):
     return render(request, 'home.html')
 
@@ -17,4 +18,9 @@ def cat_index(request):
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
     return render(request, 'cats/detail.html', {'cat': cat})
-    
+
+# CLASS BASED VIEWS
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+    success_url = '/cats/' 
