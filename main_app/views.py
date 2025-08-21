849cceecb5a0e2fea1fe0061eba1ab14c2e287ff
mohd <generalassemblyse@gmail.com>
2025-08-21 15:01:33 +0300

    add cats

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Cat
 # Create your views here.
-def home(request):
-    return HttpResponse('<h1>Home ᓚᘏᗢ</h1>')
 
+
+def home(request):
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def cat_index(request):
+    cats = Cat.objects.all()
+    return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
+    
